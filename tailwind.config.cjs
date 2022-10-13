e425c35b6598feb85a9bd399c213ca5c651f0f07
Lucas Leme <lucasratm40@gmail.com>
2022-10-13 19:58:50 -0300

    env finished

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,6 +4,25 @@ module.exports = {
     './src/**/*.tsx',
   ],
   theme: {
+    fontSize: {
+      xs: 14,
+      sm: 16,
+      medium: 18,
+      large: 20,
+      xl: 24,
+      '2-xl': 32,
+    },
+
+    color: {
+      'gray-900': '#121214',
+      'gray-800': '#202024',
+      'gray-400': '#7c7c8a',
+      'gray-200': '#c4c4cc',
+      'gray-100': '#e1e1e6',
+
+      'cyan-300': '#81d8f7',
+      'cyan-500': '#adeaff'
+    },
     extend: {
       fontFamily: {
         sans: 'Inter, sans-serif'
